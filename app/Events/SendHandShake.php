a7f6adf03d11c63ca057d65f7df1a6c066232a7f
Kamleshpaul <kamleshpaul414@gmail.com>
2021-12-30 11:59:14 +0530

    done setup basic video calll

--- app/Events/SendHandShake.php
+++ app/Events/SendHandShake.php
@@ -9,12 +9,13 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
 class SendHandShake implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $data, $senderId, $reciverId;
+    public $data, $senderId, $reciverId, $caller;
 
     /**
      * Create a new event instance.
@@ -26,6 +27,7 @@ class SendHandShake implements ShouldBroadcast
         $this->senderId = $senderId;
         $this->reciverId = $reciverId;
         $this->data = $data;
+        $this->caller = User::find($senderId);
     }
 
     /**
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,224 +1,333 @@
 <template>
-	<app-layout>
-		<template #header>
-			<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-				Laravel Vue WebRTC
-			</h2>
-		</template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Laravel Vue WebRTC
+      </h2>
+    </template>
 
-		<div class="py-12">
-			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-				<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-					<div class="p-5">
-						<h1>Users</h1>
-						<ul class="flex p-2">
-							<li
-								v-for="row in users"
-								v-show="row.id != user.id"
-								:key="row.id"
-								class="bg-gray-500 h-16 w-16 m-4 rounded-full text-white relative cursor-pointer"
-								@click="startCall(row.id)"
-							>
-								<span
-									class="absolute top-3 left-6 font-bold text-3xl adjust-margin"
-									>{{ row.name.charAt(0) }}</span
-								>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <div class="p-5">
+            <h1>Users</h1>
+            <ul class="flex p-2">
+              <li
+                v-for="row in users"
+                v-show="row.id != user.id"
+                :key="row.id"
+                class="
+                  bg-gray-500
+                  h-16
+                  w-16
+                  m-4
+                  rounded-full
+                  text-white
+                  relative
+                  cursor-pointer
+                "
+                @click="startCall(row.id)"
+              >
+                <span
+                  class="absolute top-3 left-6 font-bold text-3xl adjust-margin"
+                  >{{ row.name.charAt(0) }}</span
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
 
-		<Modal :show="ModelShow">
-			<div class="p-5">
-				<h2>{{ caller ? caller.name : "" }} is Calling...</h2>
+    <Modal :show="ModelShow">
+      <div class="p-5">
+        <h2>{{ caller ? caller.name : "" }} is Calling...</h2>
 
-				<div class="flex">
-					<button
-						@click="AnswerCall(caller)"
-						class="bg-green-300 hover:bg-green-400 text-green-800 font-bold py-2 px-4 rounded inline-flex items-center text-white m-3"
-					>
-						Answer
-					</button>
-					<button
-						@click="rejectCall"
-						class="bg-red-300 hover:bg-red-400 text-red-800 font-bold py-2 px-4 rounded inline-flex items-center text-white m-3"
-					>
-						Reject
-					</button>
-				</div>
-			</div>
-		</Modal>
+        <div class="flex">
+          <button
+            @click="AnswerCall(caller)"
+            class="
+              bg-green-300
+              hover:bg-green-400
+              text-green-800
+              font-bold
+              py-2
+              px-4
+              rounded
+              inline-flex
+              items-center
+              text-white
+              m-3
+            "
+          >
+            Answer
+          </button>
+          <button
+            @click="rejectCall"
+            class="
+              bg-red-300
+              hover:bg-red-400
+              text-red-800
+              font-bold
+              py-2
+              px-4
+              rounded
+              inline-flex
+              items-center
+              text-white
+              m-3
+            "
+          >
+            Reject
+          </button>
+        </div>
+      </div>
+    </Modal>
 
-		<Modal :show="isCallOn">
-			<div class="p-5">
-				<h2>Video Chat</h2>
-				<div class="flex">
-					<video
-						id="myVideo"
-						playsinline
-						autoplay
-						muted
-						class="w-1/2"
-					></video>
-					<video
-						id="otherVideo"
-						playsinline
-						autoplay
-						class="w-1/2"
-					></video>
-				</div>
+    <Modal :show="isCallOn">
+      <div class="p-5">
+        <h2>Video Chat</h2>
+        <div class="flex">
+          <video id="myVideo" playsinline autoplay muted class="w-1/2 mr-2"></video>
+          <video id="otherVideo" playsinline autoplay class="w-1/2"></video>
+        </div>
 
-				<button
-					@click="EndCall"
-					class="bg-red-300 hover:bg-red-400 text-red-800 font-bold py-2 px-4 rounded inline-flex items-center text-white m-3"
-				>
-					Close
-				</button>
-			</div>
-		</Modal>
-	</app-layout>
+        <button
+          @click="EndCall"
+          class="
+            bg-red-300
+            hover:bg-red-400
+            text-red-800
+            font-bold
+            py-2
+            px-4
+            rounded
+            inline-flex
+            items-center
+            text-white
+            m-3
+          "
+        >
+          Close
+        </button>
+      </div>
+    </Modal>
+  </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
 import Modal from "@/Jetstream/Modal";
 export default {
-	props: ["users", "user"],
+  props: ["users", "user"],
 
-	data() {
-		return {
-			caller: null,
-			ModelShow: false,
-			isCallOn: false,
-			offerData: null,
-			localConnection: null,
-			remoteConnection: null,
-			offer: null,
-		};
-	},
-	components: {
-		AppLayout,
-		Modal,
-	},
+  data() {
+    return {
+      servers: {
+        iceServers: [
+          {
+            urls: [
+              "stun:stun1.l.google.com:19302",
+              "stun:stun2.l.google.com:19302",
+            ],
+          },
+        ],
+        iceCandidatePoolSize: 10,
+      },
+      PC: null,
+      localStream: null,
+      remoteStream: null,
+      ModelShow: false,
+      isCallOn: false,
+      caller: null,
+    };
+  },
+  components: {
+    AppLayout,
+    Modal,
+  },
 
-	methods: {
-		startCall(id) {
-			if (id == this.user.id) {
-				alert("can't call to own");
-				return false;
-			}
-			this.isCallOn = true;
-			this.ModelShow = false;
+  methods: {
+    async startCall(id) {
+      if (id == this.user.id) {
+        alert("can't call to own");
+        return false;
+      }
+      this.isCallOn = true;
+      this.ModelShow = false;
+      try {
+        this.localStream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+      } catch (error) {
+        alert("Web cam access denied.");
+      }
+      this.remoteStream = new MediaStream();
 
-			this.localConnection.onicecandidate = (e) => {
-				this.offer = JSON.stringify(
-					this.localConnection.localDescription
-				);
-			};
-			setTimeout(() => {
-				console.log("send offer");
-				axios.post(route("handshake"), {
-					senderId: this.user.id,
-					reciverId: id,
-					_token: csrfToken,
-					data: this.offer,
-				});
-			}, 1000);
+      // Push tracks from local stream to peer connection
+      this.localStream.getTracks().forEach((track) => {
+        this.PC.addTrack(track, this.localStream);
+      });
 
-			this.localConnection
-				.createOffer()
-				.then((o) => this.localConnection.setLocalDescription(o));
-		},
+      // Pull tracks from remote stream, add to video stream
+      this.PC.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          this.remoteStream.addTrack(track);
+        });
+      };
 
-		rejectCall() {
-			this.ModelShow = false;
-		},
+      let video = document.getElementById("myVideo");
+      video.srcObject = this.localStream;
 
-		createRTCOffer(offer, reciverId) {
-			this.isCallOn = true;
-			this.ModelShow = false;
+      let otherVideo = document.getElementById("otherVideo");
+      otherVideo.srcObject = this.remoteStream;
 
-			this.remoteConnection
-				.setRemoteDescription(offer)
-				.then((a) => console.log("remote offer accept"));
+      //get latest ice candidate and sent to other party
+      this.PC.onicecandidate = (event) => {
+        if (event.candidate) {
+          axios.post(route("handshake"), {
+            senderId: this.user.id,
+            reciverId: id,
+            _token: csrfToken,
+            data: JSON.stringify({
+              type: "candidate",
+              data: event.candidate,
+            }),
+          });
+        }
+      };
 
-			//create answer
-			this.remoteConnection
-				.createAnswer()
-				.then((a) => this.remoteConnection.setLocalDescription(a))
-				.then((a) => {
-					console.log("sent answer");
-					axios.post(route("handshake"), {
-						senderId: this.user.id,
-						reciverId: reciverId,
-						_token: csrfToken,
-						data: JSON.stringify(
-							this.remoteConnection.localDescription
-						),
-					});
-				});
-		},
+      // Create offer
+      const offerDescription = await this.PC.createOffer();
+      await this.PC.setLocalDescription(offerDescription);
 
-		AnswerCall(user) {},
-		EndCall() {
-			this.isCallOn = false;
-			this.localConnection.close();
-			this.remoteConnection.close();
-		},
+      const offer = {
+        sdp: offerDescription.sdp,
+        type: offerDescription.type,
+      };
+      axios.post(route("handshake"), {
+        senderId: this.user.id,
+        reciverId: id,
+        _token: csrfToken,
+        data: JSON.stringify(offer),
+      });
+    },
 
-		init() {
-			this.localConnection = new RTCPeerConnection();
-			this.remoteConnection = new RTCPeerConnection();
-			window.remoteConnection = this.remoteConnection;
-			// listen for remote video
-			this.remoteConnection.ontrack = (e) => {
-				console.log("peer1 reciving video");
-				let video = document.getElementById("otherVideo");
-				video.srcObject = e.streams[0];
-			};
-			// set track to local video
-			navigator.mediaDevices
-				.getUserMedia({ video: true, audio: true })
-				.then((stream) => {
-					let video = document.getElementById("myVideo");
-					video.srcObject = stream;
+    rejectCall() {
+      this.ModelShow = false;
+    },
 
-					stream
-						.getTracks()
-						.forEach((track) =>
-							this.localConnection.addTrack(track, stream)
-						);
-				});
-		},
-	},
+    AnswerCall(user) {},
+    EndCall() {
+      this.isCallOn = false;
+      this.PC.close();
+    },
 
-	mounted() {
-		this.init();
-		Echo.channel(`handshake.${this.user.id}`).listen(
-			"SendHandShake",
-			(e) => {
-				const data = JSON.parse(e.data);
-				if (data.type == "offer") {
-					const offer = data;
-					this.createRTCOffer(offer, e.senderId);
-				} else {
-					const answer = data;
-					console.log("accept  answer");
-					this.localConnection
-						.setRemoteDescription(answer)
-						.then((a) => console.log("final done"));
-				}
-			}
-		);
-	},
+    async setOffer(data, offerData) {
+      console.log("answering...");
+      this.caller = data.caller;
+      this.isCallOn = true;
+      this.ModelShow = true;
+
+      try {
+        this.localStream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+      } catch (error) {
+        alert("Web cam access denied.");
+      }
+
+      this.remoteStream = new MediaStream();
+
+      // Push tracks from local stream to peer connection
+      this.localStream.getTracks().forEach((track) => {
+        this.PC.addTrack(track, this.localStream);
+      });
+
+      // Pull tracks from remote stream, add to video stream
+      this.PC.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          this.remoteStream.addTrack(track);
+        });
+      };
+
+      let video = document.getElementById("myVideo");
+      video.srcObject = this.localStream;
+
+      let otherVideo = document.getElementById("otherVideo");
+      otherVideo.srcObject = this.remoteStream;
+
+      //get latest ice candidate and sent to other party
+      this.PC.onicecandidate = (event) => {
+        if (event.candidate) {
+          axios.post(route("handshake"), {
+            senderId: this.user.id,
+            reciverId: this.caller.id,
+            _token: csrfToken,
+            data: JSON.stringify({
+              type: "candidate",
+              data: event.candidate,
+            }),
+          });
+        }
+      };
+
+      // set offer as local and generate answer
+      const offerDescription = offerData;
+      await this.PC.setRemoteDescription(
+        new RTCSessionDescription(offerDescription)
+      );
+      const answerDescription = await this.PC.createAnswer();
+      await this.PC.setLocalDescription(answerDescription);
+      const answer = {
+        type: answerDescription.type,
+        sdp: answerDescription.sdp,
+      };
+      axios.post(route("handshake"), {
+        senderId: this.user.id,
+        reciverId: this.caller.id,
+        _token: csrfToken,
+        data: JSON.stringify(answer),
+      });
+    },
+    async setAnswer(answerData) {
+      const answerDescription = new RTCSessionDescription(answerData);
+      this.PC.setRemoteDescription(answerDescription);
+      console.log("answer set");
+    },
+    setCandidate(candidateData) {
+      this.PC.addIceCandidate(new RTCIceCandidate(candidateData.data));
+    },
+  },
+
+  mounted() {
+    this.PC = new RTCPeerConnection(this.servers);
+
+    Echo.channel(`handshake.${this.user.id}`).listen(
+      "SendHandShake",
+      (data) => {
+        const handShakeData = JSON.parse(data.data);
+        if (handShakeData.type == "offer") {
+          this.setOffer(data, handShakeData);
+        }
+
+        if (handShakeData.type == "answer") {
+          this.setAnswer(handShakeData);
+        }
+        if (handShakeData.type == "candidate") {
+          this.setCandidate(handShakeData);
+        }
+      }
+    );
+  },
 };
 </script>
 
 
 <style  scoped>
 .adjust-margin {
-	margin-left: -3px;
+  margin-left: -3px;
 }
 </style>
